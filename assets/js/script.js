3540eb21d410e794de6ebc6ee1d3891e1d83e7f4
ibrahimjasim <>
2023-06-20 18:10:54 +0000

    Add a score limiter fuction to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,39 @@
-
 document.addEventListener("DOMContentLoaded", function (event) {
-
     const playerText = document.querySelector("#playerText");
     const computerText = document.querySelector("#computerText");
     const resultText = document.querySelector("#resultText");
     const choiceBtns = document.querySelectorAll(".choiceBtn");
+
     let player;
     let computer;
     let computerScore = 0;
     let playerScore = 0;
-    // let result;
 
-    choiceBtns.forEach(button => button.addEventListener("click", (event) => {
-        player = event.target.getAttribute('data-option');
-        computerTurn();
-        const result = checkWinner();
-        const resultMessage = ` Comp picked:: ${computer} -- ${result}`;
-        resultText.textContent = resultMessage;
-        if (result === 'You Win!') {
-            playerScore = playerScore + 1;
-        }
-        if (result === 'You Lose!') {
-            computerScore = computerScore + 1;
-        }
-        document.getElementById('user-score').innerText = playerScore;
-        document.getElementById('computer-score').innerText = computerScore;
-    }));
+    choiceBtns.forEach((button) =>
+        button.addEventListener("click", (event) => {
+            if (playerScore < 5 && computerScore < 5) {
+                player = event.target.getAttribute("data-option");
+                computerTurn();
+                const result = checkWinner();
+                const resultMessage = ` Comp picked:: ${computer} -- ${result}`;
+                resultText.textContent = resultMessage;
+                if (result === "You Win!") {
+                    playerScore += 1;
+                }
+                if (result === "You Lose!") {
+                    computerScore += 1;
+                }
+                document.getElementById("user-score").innerText = playerScore;
+                document.getElementById("computer-score").innerText = computerScore;
+                if (playerScore === 5 || computerScore === 5) {
+                    endGame();
+                }
+            }
+        })
+    );
+
 
     function computerTurn() {
-
         const randNum = Math.floor(Math.random() * 3) + 1;
 
         switch (randNum) {
@@ -43,22 +48,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 break;
         }
     }
+
     function checkWinner() {
         if (player == computer) {
             return "Draw!";
-        }
-        else if (computer == "ROCK") {
-            return (player == "PAPER") ? "You Win!" : "You Lose!";
-        }
-        else if (computer == "PAPER") {
-            return (player == "SCISSORS") ? "You Win!" : "You Lose!";
-        }
-        else if (computer == "SCISSORS") {
-            return (player == "ROCK") ? "You Win!" : "You Lose!";
+        } else if (computer == "ROCK") {
+            return player == "PAPER" ? "You Win!" : "You Lose!";
+        } else if (computer == "PAPER") {
+            return player == "SCISSORS" ? "You Win!" : "You Lose!";
+        } else if (computer == "SCISSORS") {
+            return player == "ROCK" ? "You Win!" : "You Lose!";
         }
     }
 
-
-})
-    ;
-
+    function endGame() {
+        let message;
+        if (playerScore === 5) {
+            message = "You won the game!";
+        } else {
+            message = "You lost the game!";
+        }
+        resultText.textContent = message;
+        choiceBtns.forEach((button) => {
+            button.disabled = true;
+        });
+    }
+});
